adfbc2ffc925877db27d1b6e4b2ac08c3411ad4f
Extrode <wilsonzhou56@gmail.com>
2025-04-18 01:40:22 -0400

    GUI added with Java Swing

--- 90DayDetoxTool/src/guitest/MainGUI.java
+++ 90DayDetoxTool/src/guitest/MainGUI.java
@@ -1,10 +1,28 @@
 package guitest;
+import javax.swing.*; 
+import java.awt.*; 
 
 public class MainGUI {
 
+	public MainGUI()
+	{
+		JFrame frame = new JFrame ("90 Day Detox Tool"); 
+		frame.setSize(400, 100); 
+		frame.add(new Drawing()); 
+		frame.setVisible(true); 
+		
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World"); 
+		new MainGUI();
 	}
+	class Drawing extends JComponent 
+	{
+		public void paint(Graphics g)
+		{
+			g.drawString("Welcome to the 90 Day Detox Tool", 150, 50); 
+		}
+	}
+	
 
 }

d45d521dc58608fdae83b944e571fbb94c03142c
Extrode <wilsonzhou56@gmail.com>
2025-04-18 14:23:47 -0400

    Buttons added

--- 90DayDetoxTool/src/guitest/MainGUI.java
+++ 90DayDetoxTool/src/guitest/MainGUI.java
@@ -1,28 +1,48 @@
 package guitest;
 import javax.swing.*; 
 import java.awt.*; 
+import java.awt.event.*;
+import java.util.ArrayList; 
 
-public class MainGUI {
-
+public class MainGUI implements ActionListener 
+{
+	JButton entryCreation = new JButton("Create a new journal entry"); 
+	JButton editEntries = new JButton("Edit current journal entries"); 
+	
+	
 	public MainGUI()
 	{
 		JFrame frame = new JFrame ("90 Day Detox Tool"); 
-		frame.setSize(400, 400); 
-		frame.add(new Drawing()); 
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+		frame.setSize(800, 600); 
 		frame.setVisible(true); 
+		frame.setLayout(new FlowLayout()); 
+		//create JButtons
 		
+		
+		frame.add(entryCreation); 
+		frame.add(editEntries); 
+		
+		//add action listener
+		entryCreation.addActionListener(this);
+		editEntries.addActionListener(this); 
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		new MainGUI();
 	}
-	class Drawing extends JComponent 
+	public void actionPerformed(ActionEvent e)
 	{
-		public void paint(Graphics g)
+		if (e.getSource() == entryCreation)
 		{
-			g.drawString("Welcome to the 90 Day Detox Tool", 150, 50); 
+			
 		}
+//		else if (e.getSource() = editEntries)
+		else
+			; 
 	}
+
+		
 	
 
 }

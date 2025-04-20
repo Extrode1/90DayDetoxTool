442657e0320e2aec6b5886563dcc0a6d539e9277
Extrode <wilsonzhou56@gmail.com>
2025-04-19 21:27:00 -0400

    Text feature added

--- 90DayDetoxTool/src/guitest/MainGUI.java
+++ 90DayDetoxTool/src/guitest/MainGUI.java
@@ -29,7 +29,23 @@ public class MainGUI implements ActionListener
 		
 		//add action listener
 		entryCreation.addActionListener(this);
-		editEntries.addActionListener(this); 
+		editEntries.addActionListener(this);
+		//create JTextFields
+		day = new JTextField (15); 
+		month = new JTextField (15); 
+		year = new JTextField (15); 
+		journalEntry = new JTextField (30); 
+		
+		//create labelled fields
+		JPanel inFieldPane = new JPanel(); 
+		inFieldPane.setLayout(new GridLayout(2, 2)); 
+		inFieldPane.add(new JLabel("Day")); 
+		inFieldPane.add(day); 
+		frame.add(inFieldPane, BorderLayout.NORTH); 
+		inFieldPane.add(new JLabel("Month")); 
+		inFieldPane.add(month); 
+		
+		
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -41,9 +57,7 @@ public class MainGUI implements ActionListener
 		{
 			entryCreation.setVisible(false);
 			editEntries.setVisible(false); 
-			day = new JTextField (15); 
-			month = new JTextField (15); 
-			year = new JTextField (15); 
+				
 			
 		}
 		else if (e.getSource() == editEntries)

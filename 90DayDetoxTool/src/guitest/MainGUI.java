09d7c54d11a57292a228dabbc699a161cde99625
Extrode <wilsonzhou56@gmail.com>
2025-04-27 22:07:14 -0400

    Added displaying the date and journal entry when entry is saved

--- 90DayDetoxTool/src/guitest/MainGUI.java
+++ 90DayDetoxTool/src/guitest/MainGUI.java
@@ -9,16 +9,26 @@ public class MainGUI implements ActionListener
 	private JButton entryCreation = new JButton("Create a new journal entry"); 
 	private JButton editEntries = new JButton("Edit current journal entries");
 	private JButton back = new JButton("Back"); 
-	private JButton save = new JButton("Save Entry"); 
+	private JButton save = new JButton("Save Entry");
+	
 
 	private ArrayList <JournalEntry> entries = new ArrayList<>(); 
 	private JTextField day; 
 	private JTextField month;
 	private JTextField year;
+	private JTextField fullDate; 
 	private JTextArea journalEntry;
+	private JTextArea fullJournalEntry; 
 	private JPanel inFieldPane; 
 	private JPanel otherPane; 
 	private JPanel savePane; 
+	private JScrollPane scrollPane; 
+	private JPanel outFieldPane; 
+	private JPanel outFieldPaneTwo; 
+	
+	
+	
+	
 	
 	
 	
@@ -26,7 +36,7 @@ public class MainGUI implements ActionListener
 	{
 		JFrame frame = new JFrame ("90 Day Detox Tool"); 
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-		frame.setSize(500, 800); 
+		frame.setSize(700, 800); 
 		frame.setVisible(true); 
 		frame.setLayout(new FlowLayout()); 
 		//create JButtons
@@ -44,17 +54,23 @@ public class MainGUI implements ActionListener
 		back.addActionListener(this); 
 		
 		//create JTextFields
+		inFieldPane = new JPanel();
+		otherPane = new JPanel(); 
 		day = new JTextField (15); 
 		month = new JTextField (15); 
 		year = new JTextField (15); 
+		fullDate = new JTextField (15); 
+		fullDate.setEditable(false); 
 		journalEntry = new JTextArea (15, 30);
+		fullJournalEntry = new JTextArea(15, 30); 
+		fullJournalEntry.setEditable(false);
 		journalEntry.setLineWrap(true); 
-		JScrollPane scrollPane = new JScrollPane(journalEntry); 
+		fullJournalEntry.setLineWrap(true);
+		scrollPane = new JScrollPane(otherPane); 
 		
 		
 		//create labelled fields
-		inFieldPane = new JPanel();
-		otherPane = new JPanel(); 
+		
 		frame.add(inFieldPane); 
 		frame.add(otherPane); 
 		frame.add(scrollPane); 
@@ -73,6 +89,8 @@ public class MainGUI implements ActionListener
 		inFieldPane.add(year); 
 		year.addActionListener(this); 
 		
+		
+		otherPane.setLayout(new FlowLayout());
 		otherPane.add(new JLabel("Journal Entry")); 
 		otherPane.add(journalEntry); 
 //		journalEntry.addActionListener(this); 
@@ -82,9 +100,22 @@ public class MainGUI implements ActionListener
 		savePane.setLayout(new FlowLayout()); 
 		save.addActionListener(this); 		
 		savePane.add(save); 
-		frame.add(savePane, BorderLayout.SOUTH); 
+		frame.add(savePane, BorderLayout.SOUTH);
 		
+		//display date
+	
+		outFieldPane = new JPanel(); 
+		outFieldPane.setLayout(new GridLayout(1, 2));
+		outFieldPane.add(new JLabel("Date")); 
+		outFieldPane.add(fullDate);
+		frame.add(outFieldPane, BorderLayout.SOUTH); 
 		
+		//display journal entry
+		outFieldPaneTwo = new JPanel(); 
+		outFieldPaneTwo.setLayout(new GridLayout(1, 2));
+		outFieldPaneTwo.add(new JLabel("Journal Entry")); 
+		outFieldPaneTwo.add(fullJournalEntry);
+		frame.add(outFieldPaneTwo); 
 		
 		
 	}
@@ -102,6 +133,8 @@ public class MainGUI implements ActionListener
 			inFieldPane.setVisible(true);
 			otherPane.setVisible(true); 
 			savePane.setVisible(true);
+			outFieldPane.setVisible(true);
+			outFieldPaneTwo.setVisible(true);
 				
 			
 		}
@@ -113,6 +146,8 @@ public class MainGUI implements ActionListener
 			inFieldPane.setVisible(true);
 			otherPane.setVisible(true); 
 			savePane.setVisible(true);
+			outFieldPane.setVisible(true);
+			outFieldPaneTwo.setVisible(true);
 
 		}
 		else if (e.getSource() == back)
@@ -123,7 +158,19 @@ public class MainGUI implements ActionListener
 			inFieldPane.setVisible(false);
 			otherPane.setVisible(false); 
 			savePane.setVisible(false);
+			outFieldPane.setVisible(false);
+			outFieldPaneTwo.setVisible(false);
+			
 
+		}
+		else if (e.getSource() == save)
+		{
+			String fullString = month.getText().trim() + "/" + day.getText().trim() + "/" + year.getText().trim();
+			fullDate.setText(fullString); 
+			String entryFullString = journalEntry.getText().trim(); 
+			fullJournalEntry.setText(entryFullString); 
+			
+			
 		}
 		else
 			; 

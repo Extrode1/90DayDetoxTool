19536dee7624d99686d316fe6764d7de463c2aab
wilso <wilsonzhou56@gmail.com>
2025-05-07 16:40:10 -0400

    Added text for the main menu

--- 90DayDetoxTool/src/guitest/MainGUI.java
+++ 90DayDetoxTool/src/guitest/MainGUI.java
@@ -23,20 +23,37 @@ public class MainGUI implements ActionListener
 	private JScrollPane scrollPane; 
 	private JPanel outFieldPane; 
 	private JPanel outFieldPaneTwo;
+	
+	//create additional JLabel
+	private JLabel mainMenuLabel; 
+	private JPanel mainPane; 
+	private JLabel mainMenuLabel2; 
+	
 		
 	public MainGUI()
 	{
 		JFrame frame = new JFrame ("90 Day Detox Tool"); 
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-		frame.setSize(700, 800); 
+		frame.setSize(1000, 800); 
 		frame.setVisible(true); 
 		frame.setLayout(new FlowLayout()); 
-		//create JButtons
+		//create JButtons		
+		 
+		back.setVisible(false);
 		
+		//create a main menu label 
+		mainPane = new JPanel(new BorderLayout()); 
+		mainMenuLabel = new JLabel("Welcome to the 90 Day Detox Tool!"); 
+		mainMenuLabel.setFont(new Font("Calibri",  Font.BOLD, 40)); 
+		mainMenuLabel2 = new JLabel("This tool will allow you to create journal entries to keep track of your progress and record your thoughts and feelings when you are doing the 90 day detox. ");
+		mainMenuLabel2.setFont(new Font ("Calibri", Font.ITALIC, 15)); 
+		
+		mainPane.add(mainMenuLabel, BorderLayout.NORTH); 
+		mainPane.add(mainMenuLabel2, BorderLayout.SOUTH); 
+		frame.add(mainPane); 
 		frame.add(entryCreation); 
 		frame.add(editEntries); 
-		frame.add(back); 
-		back.setVisible(false);
+		frame.add(back);
 		
 		//add action listener
 		entryCreation.addActionListener(this);
@@ -125,6 +142,7 @@ public class MainGUI implements ActionListener
 			savePane.setVisible(true);
 			outFieldPane.setVisible(false);
 			outFieldPaneTwo.setVisible(false);
+			mainPane.setVisible(false);
 				
 			
 		}
@@ -138,6 +156,7 @@ public class MainGUI implements ActionListener
 			savePane.setVisible(true);
 			outFieldPane.setVisible(false);
 			outFieldPaneTwo.setVisible(false);
+			mainPane.setVisible(false);
 
 		}
 		else if (e.getSource() == back)
@@ -150,6 +169,7 @@ public class MainGUI implements ActionListener
 			savePane.setVisible(false);
 			outFieldPane.setVisible(false);
 			outFieldPaneTwo.setVisible(false);
+			mainPane.setVisible(true);
 			
 
 		}

9c31112d36050d0376b9d65109a139f19577fa13
wilso <wilsonzhou56@gmail.com>
2025-06-18 16:39:25 -0400

    Created Function_Edit file

---
+++ 90DayDetoxTool/src/guitest/Function_Edit.java
@@ -0,0 +1,16 @@
+package guitest;
+
+public class Function_Edit {
+	MainGUI gui; 
+	public Function_Edit(MainGUI gui)
+	{
+		this.gui = gui; 
+	}
+	public void undo() {
+		gui.undo.undo(); 
+	}
+	public void redo() {
+		gui.undo.redo(); 
+	}
+
+}
